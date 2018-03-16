3cb1c13249d07f52dba01a7cd5ead8cb20218e02
Esa Firman <esafirm21@gmail.com>
2018-03-16 12:53:07 +0700

    Add verify lambda test

---
+++ src/test/kotlin/nolambda.playground/VerifyLambda.kt
@@ -0,0 +1,42 @@
+package nolambda.playground
+
+import io.kotlintest.mock.`when`
+import io.kotlintest.mock.mock
+import io.kotlintest.specs.StringSpec
+import org.mockito.Mockito
+
+typealias Callback = (Boolean) -> Unit
+
+class VerifyLambda : StringSpec({
+
+    "Verify Lamdba" {
+        val producer = mock<BooleanProducer>()
+        val tested = SomeClass(producer)
+
+        `when`(producer.getBoolean()).thenReturn(true)
+
+        val success = mock<Callback>()
+        val error = mock<Callback>()
+
+        tested.checkService(success, error)
+
+        Mockito.verify(success).invoke(true)
+    }
+
+})
+
+class BooleanProducer {
+    fun getBoolean() = false
+}
+
+class SomeClass(val producer: BooleanProducer) {
+
+    fun checkService(success: Callback, error: Callback) {
+        if (producer.getBoolean()) {
+            success(true)
+        } else {
+            error(false)
+        }
+    }
+
+}
