72529fd58f1c61e03c298b9f73b86f4f36e0dcfc
Esa Firman <esafirm21@gmail.com>
2019-05-13 09:19:04 +0700

    Simple flow test

---
+++ src/test/kotlin/nolambda.playground/coroutine/BasicFlowTest.kt
@@ -0,0 +1,47 @@
+package nolambda.playground.coroutine
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+import kotlinx.coroutines.channels.consume
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+@Suppress("EXPERIMENTAL_API_USAGE")
+class FlowSpec : StringSpec({
+
+    "Simple Flow" {
+        val flow = flow {
+            emit(1)
+        }
+
+        val secondFlow = flowOf(1, 2, 3)
+
+        runBlocking {
+            flow.collect {
+                it shouldBe 1
+            }
+
+            val result = mutableListOf<Int>()
+            secondFlow.collect {
+                result.add(it)
+            }
+
+            result.size shouldBe 3
+        }
+    }
+
+    "Test producer" {
+        runBlocking {
+            val producer = produce {
+                (0..10).forEach {
+                    send(it)
+                }
+            }
+            producer.consume { println("Consumed") }
+            producer.isEmpty shouldBe false
+        }
+    }
+})
